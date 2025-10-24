d7e59e6abd86728d132b340c5a72e468eb4842d8
jineykimdev <jineykim.dev@gmail.com>
2025-10-24 11:01:27 +0900

    solve problems before test

--- problems.js
+++ problems.js
@@ -1,4 +1,4 @@
-/* 
+/*
     Original:
     Write a function `plusFive` that takes in a number as an argument and
     returns the sum of that number and 5.
@@ -7,26 +7,28 @@
     Write a function `plusTen` that takes in a number as an argument and
     returns the sum of that number and 10.
 */
-function plusFive(num) {
-    return num + 5;
+function plusTen(num) {
+    return num + 10;
 }
 
 
 /*
     Original:
-    Write a function `printFives(max)` that prints out the multiples of 5 that 
+    Write a function `printFives(max)` that prints out the multiples of 5 that
     are less than max.
 
     New:
     Write a function `returnSevens(max)` that returns an array that contains
     multiples of 7 that are less than max.
 */
-function printFives(max) {
+function returnSevens(max) {
+    let multiplesOfSeven = [];
     for (let i = 0; i < max; i++) {
-        if (i % 5 === 0) {
-            console.log(i);
+        if (i % 7 === 0) {
+            multiplesOfSeven.push(i);
         }
     }
+    return multiplesOfSeven;
 }
 
 
@@ -35,7 +37,7 @@ function printFives(max) {
     Write a function named `eitherStringIncluded(sentence, word1, word2)` that
     accepts a sentence and two words as arguments. The `eitherStringIncluded`
     function should return `true` if *either* `word1` or `word2` is found in
-    the sentence, and `false` if neither is found. 
+    the sentence, and `false` if neither is found.
 
     New:
     Write a function named `bothStringsIncluded(sentence, word1, word2)` that
@@ -43,8 +45,8 @@ function printFives(max) {
     function should return `true` if *both* `word1` and `word2` are found in
     the sentence, and `false` if neither or only 1 is found.
 */
-function eitherStringIncluded(sentence, word1, word2) {
-    return sentence.includes(word1) || sentence.includes(word2);
+function bothStringsIncluded(sentence, word1, word2) {
+    return sentence.includes(word1) && sentence.includes(word2);
 }
 
 
@@ -58,12 +60,12 @@ function eitherStringIncluded(sentence, word1, word2) {
     returns the product of all the numbers. The product of an array  is the number
     you get when you multiply all the numbers together.
 */
-function sumArray(arr) {
-    let sum = 0;
+function productArray(arr) {
+    let product = 1;
     for (let i = 0; i < arr.length; i++) {
-        sum += arr[i];
+        product *= arr[i];
     }
-    return sum;
+    return product;
 }
 
 
@@ -76,8 +78,8 @@ function sumArray(arr) {
     Write a function `fiveAndEleven` that takes in a number and returns `true`
     if the number is divisible by BOTH 5 and 11 and `false` otherwise.
 */
-function threeOrSeven(num) {
-    return num % 3 === 0 || num % 7 === 0;
+function fiveAndEleven(num) {
+    return num % 5 === 0 && num % 11 === 0;
 }
 
 
@@ -90,11 +92,11 @@ function threeOrSeven(num) {
     Write a function, `countConsonants(word)`, that takes in a string word and
     returns the number of consonants in the word.
 */
-function countVowels(word) {
-    const vowels = ["a", "e", "i", "o", "u"];
+function countConsonants(word) {
+    const consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
     let count = 0;
     for (let i = 0; i < word.length; i++) {
-        if (vowels.includes(word[i])) {
+        if (consonants.includes(word[i])) {
             count++;
         }
     }
@@ -115,11 +117,19 @@ function countVowels(word) {
     string.
 
     Strings are immutable, so here are some tools you may find useful. The
-    `.split('')` function on strings to make a copy of the string as an array. 
+    `.split('')` function on strings to make a copy of the string as an array.
     The `.join('')` function joins the elements in an array into a string.
 */
-function whisper(str) {
-    return str.toLowerCase();
+function alternatingLetters(str) {
+    let strArray = str.split('');
+    strArray[0] = strArray[0].toLowerCase();
+    for (let index = 1; index < strArray.length; index++) {
+        if (index % 2 === 0) {
+            strArray[index] = strArray[index].toLowerCase();
+        }
+        strArray[index] = strArray[index].toUpperCase();
+    }
+    return strArray.join('');
 }
 
 
@@ -130,5 +140,5 @@ module.exports = {
     productArray,
     fiveAndEleven,
     countConsonants,
-    alternatingLetters   
+    alternatingLetters
 }
